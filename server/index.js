39d747bf251c37c1624c03ebddb0bad835e262b5
YeonHo0718 <ianshim0718@naver.com>
2026-04-23 13:00:00 +0900

    feat: integrate crawlers into user statistics API

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const github = require('./crawlers/github');
+const solvedac = require('./crawlers/solvedac');
 
 dotenv.config();
 
@@ -7,6 +9,29 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
+
+// 유저 통합 역량 데이터 조회 API
+app.get('/api/user/:githubId/:bojId', async (req, res) => {
+  const { githubId, bojId } = req.params;
+  
+  try {
+    const [ghProfile, ghStats, bojInfo] = await Promise.all([
+      github.getUserProfile(githubId),
+      github.getUserContributionStats(githubId),
+      solvedac.getUserInfo(bojId)
+    ]);
+
+    res.json({
+      success: true,
+      data: {
+        github: { ...ghProfile, ...ghStats },
+        boj: bojInfo
+      }
+    });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
 
 app.get('/health', (req, res) => {
   res.json({ status: 'ok', message: 'Link_U Backend Server is running' });
