3d9c51f34b26dc34c3b421350317f422a14f7833
jhkimo <zxciop908@gmail.com>
2026-05-26 14:16:45 +0900

    feat: improve external account linking

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
-const { startGoogleLogin, handleGoogleCallback } = require('../controllers/authController');
+const {
+  handleGithubCallback,
+  handleGoogleCallback,
+  startGithubLink,
+  startGoogleLogin,
+} = require('../controllers/authController');
 
 const router = express.Router();
 
 router.get('/google', startGoogleLogin);
 router.get('/google/callback', handleGoogleCallback);
+router.get('/github', startGithubLink);
+router.get('/github/callback', handleGithubCallback);
 
 module.exports = router;
